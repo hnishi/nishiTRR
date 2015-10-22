91e94c2ad7c29c0c556071923e7e0a6cd1de8e6c
hnishi <nishigami2509@gmail.com>
2015-10-22 20:50:06 +0900

    modified:   getene.py

--- getene.py
+++ getene.py
@@ -2,6 +2,10 @@ import struct, sys, os
 import numpy as np
 
 single = np.fromfile("128390.fb.ene")
+"""
 for i in single:
   print i
-
+"""
+for i,v in enumerate(single):
+  if i%5000 == 0:
+    print v ,i
